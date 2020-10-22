f54a35a43ab734fdbd93578c2934cbe6f7bd31b8
Erik <nergaard.erik@gmail.com>
2020-10-22 16:59:44 +0200

    Nytt forsøk på innloggingsfunksjon. Compilerer, men kjører ikke

---
+++ src/main/java/com/servlet/Controller.java
@@ -0,0 +1,57 @@
+package com.servlet;
+
+import tools.DbTool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.*;
+
+public class Controller extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+            String _username = request.getParameter("vcbrukernavn");
+            String _password = request.getParameter("vcpassord");
+            String action = request.getParameter("action");
+            Connection db = null;
+            PreparedStatement pwm = null;
+
+            try {
+         //   String in = req.getParameter("in");
+           // if (action.contains("login"))
+
+
+                if (_username != null && action.contains("Login")) {
+                    //    try {
+                    db = DbTool.getINSTANCE().dbLoggIn(out);
+                    String Query = "Select * from Roprosjekt.Brukerinfo where Brukernavn=? and Passord=?";
+                    //  PreparedStatement psm = conn.prepareStatement(Query);
+                    pwm.setString(1, _username);
+                    pwm.setString(2, _password);
+                    ResultSet rs = pwm.executeQuery();
+                    if (rs.next()) {
+                        response.sendRedirect("AdminSide.jsp");
+                    } else
+                        response.sendRedirect("Login.jsp");
+                } else
+                    System.out.println("Eriks Error");
+                } catch (Exception ex) {
+                    out.println("Exception :" + ex.getMessage());
+                }
+            }
+
+        }
+
+    }
+
+
--- src/main/webapp/Login.jsp
+++ src/main/webapp/Login.jsp
@@ -7,28 +7,32 @@
 --%>
 
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-         pageEncoding="ISO-8859-1"%>
+         pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-    <title>Guru Login Form</title>
+    <title>Login</title>
+    <link href="test.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-<form action="<%=request.getContextPath()%>/Login" method="post">
+<form action="Controller" method="post">
     <table style="with: 100%">
 
         <tr>
-            <td>UserName</td>
-            <td><input type="text" name="brukernavn" /></td>
+            <th align="right"> Brukernavn: </th>
+            <td><input type="text" name="vcbrukernavn" placeholder="Brukernavn"></td>
         </tr>
         <tr>
-            <td>Password</td>
-            <td><input type="passord" name="passord" /></td>
+            <th align="right"> Passord: </th>
+            <td colspan="2" align="right">
+                <input type="passord" name="vcpassord" placeholder="Passord" >
+            </td>
+
         </tr>
     </table>
  <!--   <button class="button buttonHent"> <a href="AdminSide.jsp"> Login</a></button> /> -->
-<br> <input type="submit" value="Submit" />
+<br> <input type="submit" value="Logg inn" class="btn" >
 </form>
 
 
