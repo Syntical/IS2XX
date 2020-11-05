1f2604586e6e735cde9a74a7db6a67b52189c843
Perry097 <perry_olsen@hotmail.no>
2020-11-05 13:24:44 +0100

    Oppdatert søkefunksjonen

--- src/main/java/tools/repository/SearchRepo.java
+++ src/main/java/tools/repository/SearchRepo.java
@@ -28,14 +28,14 @@ public class SearchRepo {
         try {
             db = DbTool.getINSTANCE().dbLoggIn(p);
             ResultSet rs = null;
-            String query = "SELECT * FROM utovere where fornavn = ?";
+            String query = "SELECT * FROM utovere JOIN testregister where fornavn = ?";
             prepareStatement = db.prepareStatement(query);
             prepareStatement.setString(1, fornavn);
             rs = prepareStatement.executeQuery();
             while (rs.next()) {
                 UtoevereModel utoever = new UtoevereModel(rs.getString("fornavn"), rs.getString("etternavn"), rs.getString("fodselsdato"), rs.getString("hoyde"), rs.getString("vekt"),
                         rs.getString("5000_watt"), rs.getString("5000_tid"), rs.getString("3000_sek"), rs.getString("3000_tid"), rs.getString("3000_lop_tid"), rs.getString("2000_watt"), rs.getString("2000_tid"),
-                        rs.getString("60_watt"), rs.getString("kropps_hev_stk"), rs.getString("Sargeant_stk"), rs.getString("beveg_stk"), rs.getString("ligg_ro"), rs.getString("ligg_ro_åst"), rs.getString("ligg_ro_kg"), rs.getString("kneboy_pst"),
+                        rs.getString("60_watt"), rs.getString("kropps_hev_stk"), rs.getString("Sargeant_stk"), rs.getString("beveg_stk"), rs.getString("ligg_ro"), rs.getString("ligg_ro_pst"), rs.getString("ligg_ro_kg"), rs.getString("kneboy_pst"),
                         rs.getString("kneboy_kg"), rs.getString("totalscore"));
                 toReturn.add(utoever);
             }
